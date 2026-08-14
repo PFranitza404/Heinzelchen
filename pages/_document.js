f24fa9b5f71b8cebb04bdb585cbad69526efad3e
PFranitza404 <paul.franitza@icloud.com>
2026-08-14 17:18:37 +0200

    Self-host Heinzelchen brand fonts

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,22 @@ export default function Document() {
         <link rel="shortcut icon" href="/favicon.ico?v=5" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png?v=5" />
         <link rel="apple-touch-icon" href="/apple-touch-icon.png?v=5" />
-        <link rel="stylesheet" href="/assets/styles.css?v=footer-layout-20260813" />
+        <link
+          rel="preload"
+          href="/assets/fonts/fraunces-variable.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link
+          rel="preload"
+          href="/assets/fonts/lora-variable.ttf"
+          as="font"
+          type="font/ttf"
+          crossOrigin="anonymous"
+        />
+        <link rel="stylesheet" href="/assets/fonts.css?v=self-hosted-fonts-20260814" />
+        <link rel="stylesheet" href="/assets/styles.css?v=self-hosted-fonts-20260814" />
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.css" />
       </Head>
       <body>
